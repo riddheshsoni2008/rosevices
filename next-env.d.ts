7bba7bdc37255961ce7bc776f604c166bec09ba9
Riddhesh Soni <ns3401976@gmail.com>
2026-09-03 15:57:08 +0530

    Add ambient module declarations for Node built-ins in next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -4,10 +4,16 @@
 declare module "*.css";
 declare module "*.svg";
 
+declare module "http";
 declare module "fs";
 declare module "path";
+declare module "url";
+declare module "os";
+declare module "node:http";
 declare module "node:fs";
 declare module "node:path";
+declare module "node:url";
+declare module "node:os";
 
 declare var process: {
   env: { [key: string]: string | undefined };
